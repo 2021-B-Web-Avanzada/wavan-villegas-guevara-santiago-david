c771f2d444d1b5f6108957f0ce131146a5308a6d
Santiago <santiago.villegas@epn.edu.ec>
2022-01-09 11:44:24 -0500

    update/examen

--- Examen1B/instantGaiming/src/app/componentes/catalogo-juegos/catalogo-juegos/catalogo-juegos.component.ts
+++ Examen1B/instantGaiming/src/app/componentes/catalogo-juegos/catalogo-juegos/catalogo-juegos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-catalogo-juegos',
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catalogo-juegos.component.scss']
 })
 export class CatalogoJuegosComponent implements OnInit {
+  @Input()
+  id=2;
+  @Input()
+  nombre='Dying Light 2 Stay Human';
+  @Input()
+  precio=68.00;
+  @Input()
+  descuento=31;
+  @Input()
+  plataforma='steam';
+  @Input()
+  imagen="assets/imgs/dyin-light.jpg"
+  @Input()
+  tamanio="itemS"
 
   constructor() { }
 
