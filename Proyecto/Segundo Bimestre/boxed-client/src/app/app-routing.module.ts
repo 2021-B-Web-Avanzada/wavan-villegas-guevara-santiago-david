f6fb7f1ed83b6d8526e506be52934c1bf9ebfe6b
Santiago <santiago.villegas@epn.edu.ec>
2022-03-07 20:43:37 -0500

    update/ adicion firebase para autencticarse

--- Proyecto/Segundo Bimestre/boxed-client/src/app/app-routing.module.ts
+++ Proyecto/Segundo Bimestre/boxed-client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RutaInicioComponent} from "./rutas/ruta-inicio/ruta-inicio.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'inicio',
+    component:RutaInicioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
