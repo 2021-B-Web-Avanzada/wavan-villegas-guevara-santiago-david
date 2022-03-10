9518499fe3a63372e1427c0fc48e0245dbdead9a
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2022-03-09 19:00:29 -0500

    Update: boxed.service -> listarEstadosPaquetesPorUsuario
    Creacion de peticion HTTP e interfaz de estados para listar los estaods de los paquetes de un usuario

--- Proyecto/Segundo Bimestre/boxed-client/src/app/servicios/http/boxed.service.ts
+++ Proyecto/Segundo Bimestre/boxed-client/src/app/servicios/http/boxed.service.ts
@@ -7,6 +7,7 @@ import {environment} from "../../../environments/environment";
 import {usuarioInterface} from "./interfaces/usuario-interface";
 import {EmpresaJphInterface} from "../../../../../../../Deber03/AppCRUD/src/app/servicios/http/interfaces/empresa-jph.interface";
 import {paqueteInterface} from "./interfaces/paquete.interface";
+import {Estado} from "./interfaces/estado";
 
 
 @Injectable({
@@ -47,6 +48,12 @@ export class BoxedService {
           (resultadoEnData) => resultadoEnData as paqueteInterface[]
         )
       );
+  }
+
+  listarEstadosPaquetesPorUsuario(emailUsuario:string, idPaquete:string){
+    const url = environment.urlBoxed + '/usuario/' + emailUsuario + '/paquete/' + idPaquete + '/estados';
+    return this.httpCliente
+      .get<Estado>(url);
   }
 }
 
